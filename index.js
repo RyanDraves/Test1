9acaed25679a318bd7bb6221dd5e6251715a9dd9
Ryan Draves <ryantherat123@gmail.com>
2017-04-03 21:59:06 -0400

    Update JS iFrame Solution

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ function newEvent(link,subTitle,description) {
 	editLink = link;
 	split = editLink.split("");
 	for (i=0;i<split.length;i++) {
-		if (editLink[i] == "w" && editLink[i+1] == "a" && editLink [i+2] == "t") {
-			editLink[i] = "e";
-			editLink[i+1] = "m";
-			editLink[i+2] = "b";
-			editLink[i+3] = "e";
-			editLink[i+4] = "d";
-			editLink[i+5] = "/";
-			editLink[i+6] = "";
-			editLink[i+7] = "";
+		if (split[i] == "w" && editLink[i+1] == "a" && editLink [i+2] == "t") {
+			split[i] = "e";
+			split[i+1] = "m";
+			split[i+2] = "b";
+			split[i+3] = "e";
+			split[i+4] = "d";
+			split[i+5] = "/";
+			split[i+6] = "";
+			split[i+7] = "";
 		}
 	}
 	editLink = split.join("");
@@ -40,5 +40,9 @@ $(document).ready(function() {
 		$("#counter").html(count);
 		$("#counter").css("font-size", 0);
 	});
-	newEvent("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "Rick Roll", "You've been rolled");
+	newEvent("https://www.youtube.com/watch?v=YKJ1zhEBHEY","The Lecture Series: March 20, Dr. Joe Pole","On March 20, The Science Squad will host our fifth lecture in the Lecture Series! We will be featuring Dr. Joe Pole, a practicing optometrist at the Ferris State University. Dr. Schonbaum will be discussing his experience with scleral and GP lens research!");
+	newEvent("https://www.youtube.com/watch?v=a_RMDQyddHo","Lecture Series #4: Kelley Templin","Kelley Templin is an Environmental Health and Safety Professional with Cargill in Hersey, MI. She has over 12 years experience in Environmental Health and Safety. Kelley earned a degree in Industrial Chemistry Technology from Ferris State University. She is a licensed Medical First Responser, a member of the American Chemical Society, and a member of the American Society of Safety Engineers.");
+	newEvent("https://www.youtube.com/watch?v=khsA4bvnsvg","Lecture Series #3: Dr. Chris Schonbaum","On February 27, The Science Squad will host our third lecture in the Lecture Series! We will be featuring Dr. Chris Schonbaum, a practicing geneticist at the University of Chicago. Dr. Schonbaum will be discussing his experience with CRISPR gene therapy!");
+	newEvent("https://www.youtube.com/watch?v=BhPUg-YdEvE","Lecture Series #2: Mr. Paul Filio","On Feb. 20, The Science Squad will host our second lecture in the Lecture Series! We will be featuring Mr. Paul Filio, a political scientist affiliated with Northwestern University's GLL. He will be discussing the merits of history and politics as a scientific subject!");
+	newEvent("https://www.youtube.com/watch?v=dsdlBVN3Sac", "Lecture Series #1: Dr. Anil Venkatesh", "On February 13, The Science Squad will host our first lecture in the Lecture Series! We will be featuring Dr. Anil Venkatesh, a mathematician/engineer with degrees from UPenn and Duke. Dr. Venkatesh will be discussing his research on the mathematics of music theory!");
 });
